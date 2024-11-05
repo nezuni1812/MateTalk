dc21b8cc04138b09e29813add14417adb961e2a9
nezuni1812 <tranngocuyennhi1812@gmail.com>
2024-11-05 15:17:34 +0700

    Initial commit

---
+++ src/MateTalk/Controller.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MateTalk;
+
+/**
+ *
+ * @author Uyen Nhi
+ */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage; 
+
+public class Controller {
+
+    @FXML
+    private TextField tfName;
+    
+    private Stage mainWindow; // Define mainWindow as a variable of type Stage
+
+    public void setMainWindow(Stage mainWindow) {
+        this.mainWindow = mainWindow;
+    }
+
+
+    @FXML
+    void btnOkDetect(ActionEvent event) {
+        String title = tfName.getText();
+        mainWindow.setTitle(title);
+    }
+
+}
---
+++ src/MateTalk/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MateTalk;
+
+/**
+ *
+ * @author Uyen Nhi
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainscreen.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        controller.setMainWindow(primaryStage);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
